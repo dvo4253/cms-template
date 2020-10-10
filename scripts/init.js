7f00ed5b54ee4f25a519e7b87b0cbb80448c85b6
Nathan Dvorachek <dvo4253@users.noreply.github.com>
2020-10-09 22:34:06 -0500

    update package.json

--- scripts/init.js
+++ scripts/init.js
@@ -1,9 +1,14 @@
+const util = require('util');
 const { ROOT_DIR } = require('./constants');
-const exec = util.promisify(require('child_process').exec); const runSanityInit = require('./init-sanity');
-const runGatsbyInit = require('./init-gatsby');
 
 const run = async () => {
-    await exec('npm i -D open readfile-sync chalk', { cwd: ROOT_DIR })
+    const exec = util.promisify(require('child_process').exec);
+    await exec('npm i', { cwd: ROOT_DIR })
+    await exec('npm i -D open readline-sync chalk', { cwd: ROOT_DIR })
+
+    const runSanityInit = require('./init-sanity');
+    const runGatsbyInit = require('./init-gatsby');
+
     const options = await runSanityInit();
     await runGatsbyInit(options);
 }
